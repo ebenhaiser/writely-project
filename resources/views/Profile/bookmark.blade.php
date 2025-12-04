3a6a412cd4eeaf74333aef8c2efc8a37650b0ccd
Ebenhaiser Jonathan Caprisiano <ebenhaiser.jonathan@corp.winning-soft.com>
2025-12-04 16:05:30 +0700

    creating bookmark page

--- resources/views/Profile/bookmark.blade.php
+++ resources/views/Profile/bookmark.blade.php
@@ -3,17 +3,17 @@
     <h1 class="mt-1 mb-5">Bookmark</h1>
     <div class="card shadow">
         <div class="card-body">
-            <form action="#" method="get">
-                <div class="d-flex gap-2">
-                    <select name="category" id="" class="form-control">
-                        <option value="">All</option>
-                        {{-- @foreach ($categories as $category) --}}
-                        <option value="">
-                            {{-- {{ request('category') == $category->slug ? 'selected' : '' }}>{{ $category->name }} --}}
-                        </option>
-                        {{-- @endforeach --}}
-                    </select>
-                    <button class="btn btn-primary">Find</button>
+            <form action="" method="get">
+                <div class="d-flex gap-2 position-relative">
+                    <input type="text" name="search" id="searchInput" class="form-control"
+                    placeholder="Finding some post or user?" value="" autocomplete="off">
+                    
+                    <!-- Tombol X -->
+                    <button type="button" id="clearSearch" class="btn position-absolute" style="right: 50px; top: 50%; transform: translateY(-50%); display: {{ request('search') ? 'block' : 'none' }};">
+                        ✖
+                    </button>
+                    
+                    <button class="btn btn-primary"><i class='bx bx-search-alt-2'></i></button>
                 </div>
             </form>
         </div>
