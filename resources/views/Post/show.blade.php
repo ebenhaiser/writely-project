bb8d77cc86de32a2d5baf1adbb9d90698536342d
Ebenhaiser Jonathan Caprisiano <ebenhaiser.jonathan@corp.winning-soft.com>
2025-12-04 17:15:48 +0700

    fixing like icon

--- resources/views/Post/show.blade.php
+++ resources/views/Post/show.blade.php
@@ -64,128 +64,165 @@
                                 </a> --}}
                             </span>
                             <span class="" align="right">
-                                <button class="btn btn-outline-primary mb-1 like-btn" data-post-id="{{ $post->id }}">
+                                @if (Auth::check())
+                                {{-- <button class="btn btn-outline-primary mb-1 like-btn" data-post-id="{{ $post->id }}">
                                     <span class="like-text">{{ $post->isLikedByUser() ? 'Unlike' : 'Like' }}</span>
+                                </button> --}}
+                                <button class="btn btn-outline-primary mb-1 like-btn" data-post-id="{{ $post->id }}">
+                                    <i class="bi {{ $post->isLikedByUser() ? 'bi-hand-thumbs-up-fill' : 'bi-hand-thumbs-up' }} like-icon"></i>
                                 </button>
+                                @endif
                                 @if (Auth::check() && Auth::user()->id == $post->user_id)
-                                <a href="{{ route('post.edit', $post->slug) }}"
-                                    class="btn btn-outline-primary">Edit</a>
-                                    @endif
-                                    <p class="bi bi-hand-thumbs-up mt-3"> 
-                                        <span class="like-count">{{ $post->likes->count() }}</span>
-                                    </p>
-                                </span>
-                            </div>
+                                <a href="{{ route('post.edit', $post->slug) }}" class="btn btn-outline-primary">Edit</a>
+                                @endif
+                                <p class="mt-3 d-flex gap-3">
+                                    <i class="bi bi-hand-thumbs-up "></i>
+                                    <span class="like-count">{{ $post->likes->count() }}</span>
+                                </p>
+                            </span>
                         </div>
-                        <div class="card-body">
-                            @if ($post->thumbnail && file_exists(public_path('img/postThumbnail/' . $post->thumbnail)))
-                            <img src="{{ asset('img/postThumbnail/' . $post->thumbnail) }}" class="w-100 mb-4"
-                            alt="...">
-                            @endif
-                            <div class="ckeditor-container">{!! str_replace("\n", '<br>', e($post->content)) !!}
-                            </div>
+                    </div>
+                    <div class="card-body">
+                        @if ($post->thumbnail && file_exists(public_path('img/postThumbnail/' . $post->thumbnail)))
+                        <img src="{{ asset('img/postThumbnail/' . $post->thumbnail) }}" class="w-100 mb-4"
+                        alt="...">
+                        @endif
+                        <div class="ckeditor-container">{!! str_replace("\n", '<br>', e($post->content)) !!}
                         </div>
-                        <div class="card-footer" align="center">
-                            <p>Created at: {{ $post->created_at->format('d F Y') }}</p>
-                        </div>
+                    </div>
+                    <div class="card-footer" align="center">
+                        <p>Created at: {{ $post->created_at->format('d F Y') }}</p>
                     </div>
                 </div>
-                
             </div>
-            <div class="col-lg-4">
-                <div class="author-and-comments-section">
-                    <div class="card author-section shadow">
-                        <div class="card-header">
-                            <h5>Author</h5>
-                        </div>
-                        <div class="card-body">
-                            @php
-                            $user = $post->user;
-                            @endphp
-                            <div class="d-flex justify-content-between">
-                                <a href="{{ route('profile', $user->username) }}" style="color: inherit; text-decoration: none;">
-                                    <span class="d-flex">
-                                        <span>
-                                            <div class="me-2">
-                                                <img src="{{ asset('img/profilePicture/' . ($user->profile_picture && file_exists(public_path('img/profilePicture/' . $user->profile_picture)) ? $user->profile_picture : 'default.jpg')) }}"
-                                                alt="" class="rounded-circle  border-4 border-white-color-40">
-                                            </div>
-                                        </span>
-                                        <span class="my-auto">
-                                            <h6 class="mt-0 mb-0">{{ $user->name }}</h6>
-                                            <p class="mb-0 mt-0 text-body" style="text-decoration: none">
-                                                {{ '@' . $user->username }}
-                                            </p>
-                                        </span>
-                                    </span>
-                                </a>
-                                <span class="my-auto">
-                                    <div align="right">
-                                        @if (Auth::check() && Auth::id() !== $user->id)
-                                        <button class="btn btn-outline-primary follow-btn" data-user-id="{{ $user->id }}">
-                                            <span
-                                            class="follow-text">{{ $user->isFollowedByUser() ? 'Unfollow' : 'Follow' }}</span>
-                                        </button>
-                                        @elseif (Auth::check() && Auth::id() === $user->id)
-                                        <p class="my-auto text-muted">You</p>
-                                        @endif
-                                    </div>
-                                    <div class="mt-1" align="right">
-                                        <i>
-                                            <span class="follower-count">{{ count($user->followers) }}</span>
-                                            follower
-                                        </i>
-                                    </div>
-                                </span>
-                            </div>
-                        </div>
-                        
-                        {{-- <x-cards.user :user="$user" /> --}}
+            
+        </div>
+        <div class="col-lg-4">
+            <div class="author-and-comments-section">
+                <div class="card author-section shadow">
+                    <div class="card-header">
+                        <h5>Author</h5>
                     </div>
-                    {{-- comment --}}
-                    <!-- Input Komentar -->
-                    <div class="card comments-section shadow">
-                        <div class="card-header">
-                            <h5>Comments</h5>
+                    <div class="card-body">
+                        @php
+                        $user = $post->user;
+                        @endphp
+                        <div class="d-flex justify-content-between">
+                            <a href="{{ route('profile', $user->username) }}" style="color: inherit; text-decoration: none;">
+                                <span class="d-flex">
+                                    <span>
+                                        <div class="me-2">
+                                            <img src="{{ asset('img/profilePicture/' . ($user->profile_picture && file_exists(public_path('img/profilePicture/' . $user->profile_picture)) ? $user->profile_picture : 'default.jpg')) }}"
+                                            alt="" class="rounded-circle  border-4 border-white-color-40">
+                                        </div>
+                                    </span>
+                                    <span class="my-auto">
+                                        <h6 class="mt-0 mb-0">{{ $user->name }}</h6>
+                                        <p class="mb-0 mt-0 text-body" style="text-decoration: none">
+                                            {{ '@' . $user->username }}
+                                        </p>
+                                    </span>
+                                </span>
+                            </a>
+                            <span class="my-auto">
+                                <div align="right">
+                                    @if (Auth::check() && Auth::id() !== $user->id)
+                                    <button class="btn btn-outline-primary follow-btn" data-user-id="{{ $user->id }}">
+                                        <span
+                                        class="follow-text">{{ $user->isFollowedByUser() ? 'Unfollow' : 'Follow' }}</span>
+                                    </button>
+                                    @elseif (Auth::check() && Auth::id() === $user->id)
+                                    <p class="my-auto text-muted">You</p>
+                                    @endif
+                                </div>
+                                <div class="mt-1" align="right">
+                                    <i>
+                                        <span class="follower-count">{{ count($user->followers) }}</span>
+                                        follower
+                                    </i>
+                                </div>
+                            </span>
                         </div>
-                        <div class="card-body">
-                            <x-comments :post="$post" />
-                        </div>
+                    </div>
+                    
+                    {{-- <x-cards.user :user="$user" /> --}}
+                </div>
+                {{-- comment --}}
+                <!-- Input Komentar -->
+                <div class="card comments-section shadow">
+                    <div class="card-header">
+                        <h5>Comments</h5>
+                    </div>
+                    <div class="card-body">
+                        <x-comments :post="$post" />
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    
+    {{-- like toggle --}}
+    {{-- <script>
+        $(document).ready(function() {
+        $('.like-btn').on('click', function() {
+        let button = $(this);
+        let postId = button.data('post-id');
         
-        {{-- like toggle --}}
-        <script>
-            $(document).ready(function() {
-                $('.like-btn').on('click', function() {
-                    let button = $(this);
-                    let postId = button.data('post-id');
-                    
-                    let likeCountSpan = button.closest('.card').find('.like-count');
-                    
-                    $.ajax({
-                        url: "{{ route('like.toggle') }}",
-                        method: "POST",
-                        data: {
-                            post_id: postId,
-                            _token: "{{ csrf_token() }}"
-                        },
-                        success: function(response) {
-                            button.find('.like-text').text(response.status === 'liked' ? 'Unlike' :
-                            'Like');
-                            likeCountSpan.text(response.likes);
-                        },
-                        error: function(xhr) {
-                            if (xhr.status === 401) {
-                                // Redirect ke halaman login jika belum login
-                                window.location.href = "{{ route('login') }}";
-                            }
+        let likeCountSpan = button.closest('.card').find('.like-count');
+        
+        $.ajax({
+        url: "{{ route('like.toggle') }}",
+        method: "POST",
+        data: {
+        post_id: postId,
+        _token: "{{ csrf_token() }}"
+        },
+        success: function(response) {
+        button.find('.like-text').text(response.status === 'liked' ? 'Unlike' : 'Like');
+        likeCountSpan.text(response.likes);
+        },
+        error: function(xhr) {
+        if (xhr.status === 401) {
+        // Redirect ke halaman login jika belum login
+        window.location.href = "{{ route('login') }}";
+        }
+        }
+        });
+        });
+        });
+    </script> --}}
+    
+    <script>
+        $(document).ready(function() {
+            $('.like-btn').on('click', function() {
+                let button = $(this);
+                let postId = button.data('post-id');
+                let likeCountSpan = button.closest('.card').find('.like-count');
+                let icon = button.find('.like-icon'); // Ambil elemen icon
+                
+                $.ajax({
+                    url: "{{ route('like.toggle') }}",
+                    method: "POST",
+                    data: {
+                        post_id: postId,
+                        _token: "{{ csrf_token() }}"
+                    },
+                    success: function(response) {
+                        // Ubah icon berdasarkan status like
+                        if (response.status === 'liked') {
+                            icon.removeClass('bi-hand-thumbs-up').addClass('bi-hand-thumbs-up-fill');
+                        } else {
+                            icon.removeClass('bi-hand-thumbs-up-fill').addClass('bi-hand-thumbs-up');
                         }
-                    });
+                        likeCountSpan.text(response.likes);
+                    },
+                    error: function(xhr) {
+                        if (xhr.status === 401) {
+                            window.location.href = "{{ route('login') }}";
+                        }
+                    }
                 });
             });
-        </script>
-    </x-layout>
-    
+        });
+    </script>
+</x-layout>
